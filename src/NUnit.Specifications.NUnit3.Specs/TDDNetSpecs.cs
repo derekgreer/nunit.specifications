d792c3ad3fb3f03263c2a4408aa7601838e8bfb3
Eric Andres <emandres@gmail.com>
2015-04-20 10:40:05 -0600

    NUnit 3 compatibility changes. Added net20, net40, & net45 versions for both NUnit 2 and NUnit 3 releases.

---
+++ src/NUnit.Specifications.NUnit3.Specs/TDDNetSpecs.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace NUnit.Specifications.Specs
+{
+  public class TDDNetSpecs
+  {
+    // TestDriven.Net seems to require a using for NUnit.Framework.
+    [Category("TestDriven.Net")]
+    public class when_running_with_tddnet : ContextSpecification
+    {
+      It should_run_successfully = () => Assert.IsTrue(true);
+    }
+  }
+}
--- src/NUnit.Specifications/ObservationTestCaseData.cs
+++
@@ -1,9 +0,0 @@
-namespace NUnit.Specifications
-{
-	public class ObservationTestCaseData
-	{
-		public ContextSpecification.It Observation { get; set; }
-		public string Description { get; set; }
-		public string Name { get; set; }
-	}
-}
