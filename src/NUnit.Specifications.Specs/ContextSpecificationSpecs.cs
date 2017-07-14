17b32e754ca2c8961e27734450fddc2b36ac7988
Michael Shattuck <michael-shattuck@pluralsight.com>
2017-07-14 15:17:28 -0600

    Add visibility to exceptions that occur in Establish

--- src/NUnit.Specifications.Specs/ContextSpecificationSpecs.cs
+++ src/NUnit.Specifications.Specs/ContextSpecificationSpecs.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using NUnit.Framework;
 using NUnit.Specifications.Categories;
 
@@ -23,6 +24,62 @@ namespace NUnit.Specifications.Specs
                 () => Assert.AreEqual(_executionStack.ToArray()[_executionStack.Count - 1], "context");
 
             It should_execute_the_it_delegate_successfully = () => Assert.IsTrue(true);
+        }
+
+        [TestFixture]
+        [Subcutaneous]
+        [Subject("Context Specification")]
+        public class when_executing_a_specification_and_an_exception_is_thrown_in_the_establish
+        {
+            class SubjectUnderTest : ContextSpecification 
+            {
+                public Exception CaughtException => Exception.InnerException;
+                Establish context = () => throw new Exception("context");
+                Because of = () => throw new Exception("because");
+            }
+
+            [Test]
+            public void It_should_bubble_the_exception_from_the_establish()
+            {
+                var subjectUnderTest = new SubjectUnderTest();
+                subjectUnderTest.TestFixtureSetUp();
+                Assert.AreEqual("context", subjectUnderTest.CaughtException.Message);
+            }
+        }
+
+        [TestFixture]
+        [Subcutaneous]
+        [Subject("Context Specification")]
+        public class when_executing_a_specification_and_an_exception_is_thrown_in_the_because
+        {
+            SubjectUnderTest subjectUnderTest;
+            static bool establishHasRun;
+
+            class SubjectUnderTest : ContextSpecification 
+            {
+                public Exception CaughtException => Exception.InnerException;
+                Establish context = () => establishHasRun = true;
+                Because of = () => throw new Exception("because");
+            }
+
+            [SetUp]
+            public void SetUp()
+            {
+                subjectUnderTest = new SubjectUnderTest();
+                subjectUnderTest.TestFixtureSetUp();
+            }
+
+            [Test]
+            public void It_should_call_the_establish()
+            {
+                Assert.IsTrue(establishHasRun);
+            }
+
+            [Test]
+            public void It_should_bubble_the_exception_from_the_because()
+            {
+                Assert.AreEqual("because", subjectUnderTest.CaughtException.Message);
+            }
         }
     }
 }
