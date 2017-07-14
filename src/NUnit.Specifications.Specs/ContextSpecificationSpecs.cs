a4e21669f948cfb9b52688fd70942c9bc59f2f1b
Michael Shattuck <michael-shattuck@pluralsight.com>
2017-07-14 15:17:28 -0600

    fix: Throw exceptions which occur in the Establish delegate.
    In the event that an exception is thrown from both the Establish and the Because delegate, the exception thrown by Establish was not being thrown when executing the It delegates.  This corrects the issue by skipping the execution of the Because delegate when an exception is thrown by the Establish delegate.

--- src/NUnit.Specifications.Specs/ContextSpecificationSpecs.cs
+++ src/NUnit.Specifications.Specs/ContextSpecificationSpecs.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using NUnit.Framework;
 using NUnit.Specifications.Categories;
 
@@ -23,6 +24,53 @@ namespace NUnit.Specifications.Specs
                 () => Assert.AreEqual(_executionStack.ToArray()[_executionStack.Count - 1], "context");
 
             It should_execute_the_it_delegate_successfully = () => Assert.IsTrue(true);
+        }
+
+        [TestFixture]
+        [Subcutaneous]
+        [Subject("Context Specification")]
+        public class when_executing_a_specification_and_an_exception_is_thrown_in_the_establish : ContextSpecification
+        {
+            static SubjectUnderTest _subjectUnderTest;
+            static Exception _exception;
+
+            Establish context = () => _subjectUnderTest = new SubjectUnderTest();
+
+            Because of = () => _exception = Catch.Exception(() => _subjectUnderTest.TestFixtureSetUp());
+
+            It should_bubble_the_exception_from_the_establish = () => Assert.AreEqual("context", _subjectUnderTest.CaughtException.Message);
+
+            class SubjectUnderTest : ContextSpecification 
+            {
+                public Exception CaughtException => Exception.InnerException;
+                Establish context = () => throw new Exception("context");
+                Because of = () => throw new Exception("because");
+            }
+        }
+
+        [TestFixture]
+        [Subcutaneous]
+        [Subject("Context Specification")]
+        public class when_executing_a_specification_and_an_exception_is_thrown_in_the_because: ContextSpecification
+        {
+            static SubjectUnderTest _subjectUnderTest;
+            static Exception _exception;
+            static bool _establishHasRun;
+
+            Establish context = () => _subjectUnderTest = new SubjectUnderTest();
+
+            Because of = () => _exception = Catch.Exception(() => _subjectUnderTest.TestFixtureSetUp());
+
+            It should_call_the_establish = () => Assert.IsTrue(_establishHasRun);
+
+            It should_bubble_the_exception_from_the_because = () => Assert.AreEqual("because", _subjectUnderTest.CaughtException.Message);
+
+            class SubjectUnderTest : ContextSpecification 
+            {
+                public Exception CaughtException => Exception.InnerException;
+                Establish context = () => _establishHasRun = true;
+                Because of = () => throw new Exception("because");
+            }
         }
     }
 }
